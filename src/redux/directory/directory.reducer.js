9f623e15883b7928a464f8fc3f36e565f4cdb15e
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2019-10-29 16:40:09 +0100

    state normalization

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -4,33 +4,33 @@ const initialState = {
             title: 'hats',
             imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
             id: 1,
-            linkUrl: 'shop/hats'
+            linkUrl: 'hats'
           },
           {
             title: 'jackets',
             imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
             id: 2,
-            linkUrl: 'shop/jackets'
+            linkUrl: 'jackets'
           },
           {
             title: 'sneakers',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
             id: 3,
-            linkUrl: 'shop/sneakers'
+            linkUrl: 'sneakers'
           },
           {
             title: 'womens',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             size: 'large',
             id: 4,
-            linkUrl: 'shop/womens'
+            linkUrl: 'womens'
           },
           {
             title: 'mens',
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
             size: 'large',
             id: 5,
-            linkUrl: 'shop/mens'
+            linkUrl: 'mens'
         }
     ]
 }
