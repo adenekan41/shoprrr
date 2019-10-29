6db7b30d746d3bbd3364cf72a67dcc36adf5ab40
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2019-10-29 16:48:59 +0100

    ⚡️ setup cart actions for cart count

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,8 +1,8 @@
 import { CartActionTypes } from "./cart.types";
 
-export const toggleCartHidden = () => ({
-    type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
+// export const toggleCartHidden = () => ({
+//     type: CartActionTypes.TOGGLE_CART_HIDDEN
+// })
 export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item
