d415217fa437ee5311e75857ee77465f16a3f333
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2020-12-25 03:25:57 +0100

    ≡ƒÜÇ (chore): added update to fix compose bug

--- src/pages/category/category.container.jsx
+++ src/pages/category/category.container.jsx
@@ -1,6 +1,6 @@
 import { createStructuredSelector } from 'reselect';
 import { selectisLoading } from '../../redux/shop/shop.selectors';
-import { compose } from 'redux';
+
 import { connect } from 'react-redux';
 import WithSpinner from '../../components/with-spinner/with-spinner.components';
 import CategoryPage from './category.component';
@@ -9,7 +9,7 @@ const mapStateToProps = createStructuredSelector({
   isLoading: state => selectisLoading(state),
 });
 
-const categoryContainer = () =>
-  compose(connect(mapStateToProps), WithSpinner)(CategoryPage);
-
+// const categoryContainer = () =>
+//   compose(connect(mapStateToProps), WithSpinner)(CategoryPage);
+const categoryContainer = connect(mapStateToProps)(WithSpinner(CategoryPage))
 export default categoryContainer;
